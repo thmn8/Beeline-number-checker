3006b20faa9ec409dd8953f94061729bc7f1feb7
mygedz <mygedz@gmail.com>
2021-08-08 10:45:41 +0300

    Добавьте файлы проекта.

---
+++ Beeline number checker/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Beeline_number_checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chec_button = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // chec_button
+            // 
+            this.chec_button.Location = new System.Drawing.Point(12, 52);
+            this.chec_button.Name = "chec_button";
+            this.chec_button.Size = new System.Drawing.Size(245, 36);
+            this.chec_button.TabIndex = 1;
+            this.chec_button.Text = "Проверить";
+            this.chec_button.UseVisualStyleBackColor = true;
+            this.chec_button.Click += new System.EventHandler(this.Chec_button_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 99);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(269, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 121);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.chec_button);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button chec_button;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Beeline number checker/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Beeline_number_checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Chec_button_Click(object sender, EventArgs e)
+        {
+            //храним введенный телефон
+            string phone = textBox1.Text;
+
+            //стройка запроса
+            Uri url = new($"https://beeline.ru/menu/loginmodel/?CTN={phone}");
+
+            //запрос
+            string res = new WebClient().DownloadString(url);
+
+            if (textBox1.Text == "")
+            {
+                toolStripStatusLabel1.Text = "Номер не введен";
+            }
+            else
+            {
+                if (res.Contains("firstTimeLogin\":true"))
+                {
+                    toolStripStatusLabel1.Text = "Номер существует";
+                }
+                else
+                {
+                    res.Contains("firstTimeLogin\":string.Empty");
+                    toolStripStatusLabel1.Text = "Номер не существует";
+                }
+            }
+        }
+    }
+}
